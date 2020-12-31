8fa20a4962b2d51f40bf87fadeeb77482ac5fa21
leigh90 <zawadikibathi@gmail.com>
2020-12-31 20:05:59 +0300

    Initial commit very newbie-esque

---
+++ scripts.js
@@ -0,0 +1,30 @@
+// Get the elements with class="column"
+// var elements = document.getElementsByClassName("column");
+
+// // Declare a "loop" variable
+// var i;
+
+// // Full-width images
+// function one() {
+//   for (i = 0; i < elements.length; i++) {
+//     elements[i].style.flex = "100%";
+//   }
+// }
+
+// // Two images side by side
+// function two() {
+//   for (i = 0; i < elements.length; i++) {
+//     elements[i].style.flex = "50%";
+//   }
+// }
+
+// // Four images side by side
+// function four() {
+//   for (i = 0; i < elements.length; i++) {
+//     elements[i].style.flex = "25%";
+//   }
+// }
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
